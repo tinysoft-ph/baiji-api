ea87051b43e39dc433a50ee6928476f0b01666c1
Julius Cebreros <me@juliuscebreros.com>
2017-05-18 22:16:57 +1200

    Add api and resource declaration

--- infrastructure/test/main.tf
+++ infrastructure/test/main.tf
@@ -7,10 +7,31 @@ module "sites_table" {
   name = "sites"
 }
 
+resource "aws_api_gateway_rest_api" "baiji-test" {
+  name = "baiji-test"
+  description = "This is the baiji test api"
+}
+
+resource "aws_api_gateway_resource" "sites" {
+  rest_api_id = "${aws_api_gateway_rest_api.baiji-test.id}"
+  parent_id = "${aws_api_gateway_rest_api.baiji-test.root_resource_id}"
+  path_part = "sites"
+}
+
 module "get_sites_api" {
   source = "../base/apigateway"
+  rest_api_id = "${aws_api_gateway_rest_api.baiji-test.id}"
+  resource_id = "${aws_api_gateway_resource.sites.id}"
+}
+
+module "get_site_api" {
+  source = "../base/apigateway"
+  rest_api_id = "${aws_api_gateway_rest_api.baiji-test.id}"
+  resource_id = "${aws_api_gateway_resource.sites.id}"
 }
 
 module "create_sites_api" {
   source = "../base/apigateway"
+  rest_api_id = "${aws_api_gateway_rest_api.baiji-test.id}"
+  resource_id = "${aws_api_gateway_resource.sites.id}"
 }
